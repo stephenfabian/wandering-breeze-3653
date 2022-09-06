d38d76f8044df731d34f82974dff3f0abdcc8c8e
Stephen Fabian <fabianstephen@gmail.com>
2022-09-05 23:30:53 -0600

    adds count of contestants

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,5 +3,6 @@ class ProjectsController < ApplicationController
   def show
     @project = Project.find(params[:id])
     @challenge = Challenge.find_by(id: @project.challenge_id)
+    @contestant_count = @project.contestants.count
   end
 end
