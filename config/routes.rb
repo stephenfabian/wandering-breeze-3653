6ecfbe1d115580c8ca5b277270d5c2292a431c04
Stephen Fabian <fabianstephen@gmail.com>
2022-09-05 22:55:34 -0600

    adds feat: project show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
 
-get '/projects:id', to: 'projects#show'
+  get '/projects/:id', to: 'projects#show'
+
+
+
+
+end
